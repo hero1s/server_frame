5bda1e5f0a2022c74af3792b5df0a6e26f17f37b
toney <hero1s@qq.com>
2021-03-31 10:39:02 +0800

    修改内网消息包头

--- include/packet/inner_protobuf_pkg.h
+++ include/packet/inner_protobuf_pkg.h
@@ -17,10 +17,12 @@ using namespace Network;
 //内部数据包头------------------------------------------------------------------------------------------------------------
 typedef struct inner_header_t {
     uint32_t msgLen;        // 消息长度
-    uint8_t route;          // 路由类型
-    uint32_t routeID;       // 路由ID
-    uint32_t uin;           // uin(服务器内部转发用)
     uint16_t msgID;         // 消息id
+    uint32_t uid;           //
+    uint32_t s_ser_id;      // 源服务器
+    uint8_t s_ser_type;    // 源服务器类型
+    uint32_t d_ser_id;      // 目标服务器
+    uint8_t d_ser_type;    // 目标服务器类型
 } INNERHEAD;
 
 #define INNER_MAX_SIZE             1024*32
@@ -34,24 +36,19 @@ typedef struct {
 
 #pragma pack()
 
-class InnerDecode : public MsgDecode
-{
+class InnerDecode : public MsgDecode {
 public:
     //0收包不完全，>0 获取收到的包长，-1 错误包
-    virtual int GetPacketLen(const char * pData, uint32_t len)
-    {
-        if(len < sizeof(inner_header_t))
-        {
+    virtual int GetPacketLen(const char *pData, uint32_t len) {
+        if (len < sizeof(inner_header_t)) {
             return 0;
         }
         inner_header_t *head = (inner_header_t *) pData;
-        if(head->msgLen > INNER_MAX_SIZE)
-        {
-            LOG_ERROR("head msg len:{}",head->msgLen);
+        if (head->msgLen > INNER_MAX_SIZE) {
+            LOG_ERROR("head msg len:{}", head->msgLen);
             return -1;
         }
-        if(head->msgLen < len)
-        {
+        if (head->msgLen < len) {
             return head->msgLen;
         }
         return 0;
@@ -61,16 +58,16 @@ public:
 class pkg_inner {
 public:
     static bool
-    SendProtobufMsg(const TCPConnPtr& connPtr, const google::protobuf::Message *msg, uint16_t msgID, uint32_t uin,
-                 uint8_t route, uint32_t routeID) {
+    SendProtobufMsg(const TCPConnPtr &connPtr, const google::protobuf::Message *msg, uint16_t msgID, uint32_t uid,
+                    uint8_t s_ser_type, uint32_t s_ser_id, uint8_t d_ser_type, uint32_t d_ser_id) {
         static string sstr;
         msg->SerializeToString(&sstr);
-        return SendBuffMsg(connPtr, sstr.c_str(), sstr.length(), msgID, uin, route, routeID);
+        return SendBuffMsg(connPtr, sstr.c_str(), sstr.length(), msgID, uid, s_ser_type, s_ser_id,d_ser_type,d_ser_id);
     }
 
     static bool
-    SendBuffMsg(const TCPConnPtr& connPtr, const void *msg, uint32_t msg_len, uint16_t msgID, uint32_t uin,
-                     uint8_t route, uint32_t routeID) {
+    SendBuffMsg(const TCPConnPtr &connPtr, const void *msg, uint32_t msg_len, uint16_t msgID, uint32_t uid,
+                uint8_t s_ser_type, uint32_t s_ser_id, uint8_t d_ser_type, uint32_t d_ser_id) {
         if (connPtr == nullptr) {
             LOG_ERROR("the connPtr is nullptr");
             return false;
@@ -78,9 +75,11 @@ public:
         static inner_protobuf pkt;
         memset(&pkt, 0, sizeof(pkt));
         pkt.header.msgID = msgID;
-        pkt.header.uin = uin;
-        pkt.header.route = route;
-        pkt.header.routeID = routeID;
+        pkt.header.uid = uid;
+        pkt.header.s_ser_type = s_ser_type;
+        pkt.header.s_ser_id = s_ser_id;
+        pkt.header.d_ser_type = d_ser_type;
+        pkt.header.d_ser_id = d_ser_id;
         pkt.header.msgLen = msg_len + INNER_HEADER_SIZE;
 
         if (msg_len >= INNER_MAX_DATA_SIZE) {
@@ -89,7 +88,7 @@ public:
         }
         memcpy((void *) pkt.protobuf, msg, msg_len);
         //LOG_DEBUG("send msg:cmd:{},uid:{},len:{}",msg_type,uin,msg_len);
-        return connPtr->Send((char*) &pkt.header, msg_len + INNER_HEADER_SIZE);
+        return connPtr->Send((char *) &pkt.header, msg_len + INNER_HEADER_SIZE);
     }
 };
 
